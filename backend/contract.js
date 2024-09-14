8a2d0ab93fcaeee4ec3b1d9d757f3163ece3ab94
UdayaNarne <udayanarne08@gmail.com>
2024-09-15 00:38:29 +0530

    Updated

--- backend/contract.js
+++ backend/contract.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const data = require('../frontend/contracts/farmer.json');
 const data2=require("../frontend/sachivalayam/sachivalayams.json")
+const data3=require("../frontend/notifications.json")
 const farmers = data.farmers;
 const sachivalayams=data2.sachivalayams;
+const notifies=data3.notifications;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'backend')));
@@ -37,6 +40,10 @@ app.get('/districts/:name',(req,res)=>{
     res.json(items);
 
 });
+
+app.get('/notifications',(req,res)=>{
+    res.json(notifies);
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
