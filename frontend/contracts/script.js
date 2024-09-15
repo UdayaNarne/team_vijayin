06bf7ca24cb351dc24e3c5a09552e715599a2b5b
Neeraja G <gundubogula@gmail.com>
2024-09-15 22:39:06 +0530

    modified frontend

--- frontend/contracts/script.js
+++ frontend/contracts/script.js
@@ -5,3 +5,8 @@ document.querySelector('button').addEventListener('click',function(){
 document.getElementById('contract-grid').addEventListener('click',function(){
     window.location.href = 'farmers.html'; 
 })
+
+function displaylang() {
+    const languageList = document.querySelector('.language-list');
+    languageList.style.display = languageList.style.display === 'block' ? 'none' : 'block';
+}
---
+++ frontend/sachivalayam/script.js
@@ -0,0 +1,4 @@
+function displaylang() {
+    const languageList = document.querySelector('.language-list');
+    languageList.style.display = languageList.style.display === 'block' ? 'none' : 'block';
+}
