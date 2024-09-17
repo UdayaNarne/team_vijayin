20c89ab1a3c8a168857220a27216f6eae0b1f832
UdayaNarne <udayanarne08@gmail.com>
2024-09-17 20:57:05 +0530

    Updated

--- frontend/sachivalayam/script.js
+++ frontend/sachivalayam/script.js
@@ -3,6 +3,12 @@ function displaylang() {
     languageList.style.display = languageList.style.display === 'block' ? 'none' : 'block';
 }
 
+document.getElementById("hindi").addEventListener("click",function(){
+    window.location.href = "home_hindi.html";
+})
+document.getElementById("telugu").addEventListener("click",function(){
+    window.location.href = "home_telugu.html";
+})
 document.getElementById("acceptContract").addEventListener("click",function(){
     
 })
